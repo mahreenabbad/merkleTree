a60ef4f7aaab02c51b9db6915d71c8c02e9cfa4d
Mahreen <mahreenabbad@gmail.com>
2024-11-05 12:54:56 +0500

    Add changes

--- test/MerkleProof.js
+++ test/MerkleProof.js
@@ -71,9 +71,8 @@ describe("MerkleProof", function () {
     it("Should allow  whitelisted address to mint NFT", async function () {
       const { merkleContract, uri, tree, addr1 } = await loadFixture(runEveryTime);
 
-      const leaf = keccak256(addr1.address); // leaf for addr1
-      const proof = tree.getHexProof(leaf); // proof for addr1 (which is whitelisted)
-
+      const leaf = keccak256(addr1.address); 
+      const proof = tree.getHexProof(leaf); 
       // Expect minting to succeed for addr1 (whitelisted address)
       await expect(
         merkleContract.connect(addr1).mintNFT(addr1.address, uri, proof)
